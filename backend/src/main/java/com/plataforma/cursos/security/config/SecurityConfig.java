a7b4b2e5f533663bdcd712335c5078a7862ca7c6
Gustavo Santos <santos.gustavo.of@gmail.com>
2026-07-02 21:39:31 -0300

    backend: Correção de trailling slash de CORS

--- backend/src/main/java/com/plataforma/cursos/security/config/SecurityConfig.java
+++ backend/src/main/java/com/plataforma/cursos/security/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000", "https://project-pritz.vercel.app/"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://project-pritz.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
